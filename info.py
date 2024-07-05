b9e972d9b9f7f024f8321134da47eeb26c0684f8
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2024-07-05 08:19:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,6 +60,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002208460669'))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
